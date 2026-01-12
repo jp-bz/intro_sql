5b1a6127e05bbd9ad85d924040aa04e53e353d48
jp-bz <jbedoyaz@unal.edu.co>
2026-01-11 20:02:21 -0500

    eliminacion

--- 01_select.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM users; # Select all columns from the users table
-
-SELECT name FROM users; # Select only the name column from the users table
-
-SELECT user_id, name FROM users; # Select user_id and name columns from the users table
