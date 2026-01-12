436f8d2e038cee9433a4b266d10897bc4b782759
jp-bz <jbedoyaz@unal.edu.co>
2026-01-12 12:12:59 -0500

    actualizaciones

--- statements/03_where.sql
+++ statements/03_where.sql
@@ -1,1 +1,5 @@
 SELECT * FROM users WHERE age = 27; # Select all columns from users where age is 27
+
+SELECT name FROM users WHERE age = 27; # Select only the name column from users where age is 27
+
+
--- statements/05_like.sql
+++ statements/05_like.sql
@@ -1,2 +1,6 @@
  SELECT * FROM users WHERE email LIKE '%unal%'; # Select all columns from users where email contains 'unal'
  
+SELECT * FROM users WHERE email LIKE 'sara%' ; # Select all columns from users where email starts with 'sara'
+
+
+
