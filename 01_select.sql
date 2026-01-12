3756b01de0587918a23e3e4a3d7e04d31f8b7dc7
jp-bz <jbedoyaz@unal.edu.co>
2026-01-11 20:03:18 -0500

    creacion

---
+++ 01_select.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users; # Select all columns from the users table
+
+SELECT name FROM users; # Select only the name column from the users table
+
+SELECT user_id, name FROM users; # Select user_id and name columns from the users table
