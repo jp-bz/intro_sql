e0cca5045db41535da083348dee113a67f6723cf
jp-bz <jbedoyaz@unal.edu.co>
2026-02-11 16:09:38 -0500

    act

--- 04_tables/04_relationships.sql
+++ 04_tables/04_relationships.sql
@@ -29,3 +29,36 @@ CREATE TABLE users_languages(
     UNIQUE(user_id, language_id)
 );
 
+/* Construcción de tablas para relaciones*/
+
+INSERT INTO dni (dni_id, dni_number, user_id) VALUES (2, 22222222, 2);
+INSERT INTO dni (dni_id, dni_number, user_id) VALUES (3, 33333333, 3);
+INSERT INTO dni (dni_id, dni_number, user_id) VALUES (4, 44444444, 4);
+
+
+INSERT INTO companies (name) VALUES ('MoureDev');
+INSERT INTO companies (name) VALUES ('Apple');
+INSERT INTO companies (name) VALUES ('Google');
+
+UPDATE users SET company_id = 1 WHERE user_id = 1;
+UPDATE users SET company_id = 2 WHERE user_id = 3;
+UPDATE users SET company_id = 3 WHERE user_id = 4;
+UPDATE users SET company_id = 1 WHERE user_id = 7;
+
+INSERT INTO languages (name) VALUES ('Swift');
+INSERT INTO languages (name) VALUES ('Kotlin');
+INSERT INTO languages (name) VALUES ('JavaScript');
+INSERT INTO languages (name) VALUES ('Java');
+INSERT INTO languages (name) VALUES ('Python');
+INSERT INTO languages (name) VALUES ('C#');
+INSERT INTO languages (name) VALUES ('COBOL');
+
+
+INSERT INTO users_languages(user_id, language_id) VALUES (1, 1);
+INSERT INTO users_languages(user_id, language_id) VALUES (1, 2);
+INSERT INTO users_languages(user_id, language_id) VALUES (1, 5);
+INSERT INTO users_languages(user_id, language_id) VALUES (2, 3);
+INSERT INTO users_languages(user_id, language_id) VALUES (2, 5);
+
+
+
